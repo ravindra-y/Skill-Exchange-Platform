51bb3a3e0c6e843aaa5b601d0ea80297d2b713f5
Ravindra Yadav <ravindray.dev@gmail.com>
2026-09-21 17:26:29 +0530

    Update production API URL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://skill-exchange-platform-nypl.onrender.com/api',
   withCredentials: true,
 });
 
